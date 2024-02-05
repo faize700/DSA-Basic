d1f585255c0acfd776863c992d7cd2d86b9fbfa9
faize700 <gmfaize123@gmail.com>
2024-02-05 16:03:50 +0530

    Question 1

---
+++ PrintNumbers.js
@@ -0,0 +1,8 @@
+// Write a program that prints the numbers from 1 to 10.
+
+function printNumber(starting, ending){
+    for (let i = starting; i <= ending; i++) {
+        console.log(i);
+    }
+}
+printNumber(1, 10);
