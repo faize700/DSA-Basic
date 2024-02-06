c65f970d42632d735cdd42689adc43bee69d1168
faize700 <gmfaize123@gmail.com>
2024-02-06 19:57:56 +0530

    Ques 2

---
+++ Multipication.js
@@ -0,0 +1,9 @@
+// print multipication table of 7
+
+const multipication = nums =>{
+    for (let i = 1; i < 11; i++) {
+        console.log(`${nums} * ${i} = ${nums*i}`)
+    }
+}
+
+multipication(7);
