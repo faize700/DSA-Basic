56c4943bf7a320d2c96f392fa94fef8ddc92485a
faize700 <gmfaize123@gmail.com>
2024-02-26 08:59:10 +0530

    Ques 7

---
+++ CalculatorCelToFah.js
@@ -0,0 +1,7 @@
+// Create a function that will convert from Celsius to Fahrenheit
+
+const celsiusToFahrenheit = (C) => {
+    return `${(9/5 * C) + 32}°F`;
+}
+
+console.log(celsiusToFahrenheit(0));
