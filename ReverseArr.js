0b6ca9d6ebb19ce56c9a975a828d7c4174896bce
faize700 <gmfaize123@gmail.com>
2024-03-05 13:10:37 +0530

    Ques 21

---
+++ ReverseArr.js
@@ -0,0 +1,21 @@
+// Reverse an array
+
+const reverseArr = (arr) =>{
+    // const ans = [];
+    // for (let i = arr.length; i > 0; i--) {
+    //     ans.push(arr[i]);
+    // }
+    let s = 0;
+    let e = arr.length - 1;
+    let temp;
+    console.log(e);
+    while(s < e){
+        temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+        s++;
+        e--;
+    }
+    return arr;
+}
+console.log(reverseArr([0,1,2,3,4,5,6]));
